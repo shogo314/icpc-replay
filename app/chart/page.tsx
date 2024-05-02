1faffbfe7125e330055c8cf7d78674420af6677b
shogo314 <shg.kawasome@gmail.com>
2024-05-02 22:20:30 +0900

    v0.2.0

--- app/chart/page.tsx
+++ app/chart/page.tsx
@@ -1,67 +1,210 @@
-// yarn add chart.js react-chartjs-2
 "use client";
+"use strict";
+
+import { useSearchParams } from "next/navigation";
+import { Suspense } from "react";
+
+import standings_2023_domestic from "../../public/standings_2023_domestic.json";
+import standings_2023_yokohama from "../../public/standings_2023_yokohama.json";
+import standings_2023_asia from "../../public/standings_2023_asia.json";
+type contest_str = "standings_2023_domestic" | "standings_2023_yokohama" | "standings_2023_asia";
+
+const json_data = {
+  standings_2023_domestic,
+  standings_2023_yokohama,
+  standings_2023_asia
+} as {
+  [key: string]: {
+    ContestData: {
+      UnitTime: string;
+      Penalty: number;
+      Duration: number;
+      TaskInfo: string[];
+    };
+    StandingsData: {
+      Rank: number;
+      TotalResult: {
+        Score: number;
+        Penalty: number;
+      };
+      TeamName: string;
+      University: string;
+      TaskResults: {
+        // [key: string]: { なぜかエラーになる
+        //   Elapsed: number;
+        //   Score: number;
+        //   Penalty: number;
+        // };
+        [key: string]: any;
+      }
+    }[];
+  };
+};
+
 import { Line } from "react-chartjs-2";
 import {
-    Chart as ChartJS,
-    CategoryScale,
-    LinearScale,
-    PointElement,
-    LineElement,
-    Title,
-    Tooltip,
-    Legend,
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+  scales,
 } from "chart.js";
 
 ChartJS.register(
-    CategoryScale,
-    LinearScale,
-    PointElement,
-    LineElement,
-    Title,
-    Tooltip,
-    Legend
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
 );
 
-function MultiLineChart() {
-    const options = {
-        responsive: true,
-        plugins: {
-            title: {
-                display: true,
-                text: "グラフタイトル",
-            },
-        },
-    };
-    const data = {
-        labels: ["January", "February", "March", "April", "May"],
-        datasets: [
-            {
-                label: "First Dataset",
-                data: [12, 19, 3, 5, 2],
-                fill: false,
-                backgroundColor: "rgba(75,192,192,0.4)",
-                borderColor: "rgba(75,192,192,1)",
-                tension: 0.1,
-            },
-            {
-                label: "Second Dataset",
-                data: [22, 29, 5, 5, 20],
-                fill: false,
-                backgroundColor: "rgba(255,99,132,0.4)",
-                borderColor: "rgba(255,99,132,1)",
-                tension: 0.1,
-            },
-        ],
-    };
-    return <Line data={data} options={options} />;
-};
+function rank(contest: contest_str, id: number, time: number): number {
+  var score: number = 0;
+  var penalty: number = 0;
+  var cnt = 0;
+  for (const p in json_data[contest].StandingsData[id].TaskResults) {
+    if (json_data[contest].StandingsData[id].TaskResults[p].Score > 0 && json_data[contest].StandingsData[id].TaskResults[p].Elapsed <= time) {
+      score += json_data[contest].StandingsData[id].TaskResults[p].Score;
+      penalty += json_data[contest].StandingsData[id].TaskResults[p].Elapsed;
+      penalty += json_data[contest].StandingsData[id].TaskResults[p].Penalty * json_data[contest].ContestData.Penalty;
+    }
+  }
+  for (let i = 0; i < json_data[contest].StandingsData.length; i++) {
+    if (i == id) continue;
+    var tmp_score: number = 0;
+    var tmp_penalty: number = 0;
+    for (const p in json_data[contest].StandingsData[i].TaskResults) {
+      if (json_data[contest].StandingsData[i].TaskResults[p].Score > 0 && json_data[contest].StandingsData[i].TaskResults[p].Elapsed <= time) {
+        tmp_score += json_data[contest].StandingsData[i].TaskResults[p].Score;
+        tmp_penalty += json_data[contest].StandingsData[i].TaskResults[p].Elapsed;
+        tmp_penalty += json_data[contest].StandingsData[i].TaskResults[p].Penalty * json_data[contest].ContestData.Penalty;
+      }
+    }
+    if (tmp_score > score || (tmp_score == score && tmp_penalty < penalty)) {
+      cnt++;
+    }
+  }
+  return cnt + 1;
+}
 
-export default function Chart() {
-    return (<div>
+function Chart(contest: contest_str, team: string, id: number) {
+  var labels: number[] = [];
+  var data: number[] = [];
+  var tmp = {
+    labels,
+    datasets: [{
+      label: team,
+      data,
+      fill: false,
+      backgroundColor: "rgba(75,192,192,0.4)",
+      borderColor: "rgba(75,192,192,1)",
+      tension: 0.1,
+    }]
+  };
+  for (let i = 0; i <= json_data[contest].ContestData.Duration; i++) {
+    tmp.labels.push(i);
+    tmp.datasets[0].data.push(rank(contest, id, i));
+  }
+  const options = {
+    responsive: true,
+    scales: {
+      y: {
+        reverse: true,
+      },
+    },
+    plugins: {
+      legend: {},
+      title: {
+        display: false,
+        text: "グラフタイトル",
+      },
+      labels: {},
+      tooltip: {},
+    },
+  };
+  return (
+    <div>
+      <a href="/">
+        Home
+      </a><br />
+      contest: {contest}<br />
+      team: {team}<br />
+      university: {json_data[contest].StandingsData[id].University}<br />
+      <Line data={tmp} options={options} />
+    </div>
+  );
+}
+
+function Query() {
+  const searchParams = useSearchParams();
+  const contest = searchParams.get("contest");
+  const team = searchParams.get("team");
+  if (contest == null || team == null) {
+    const getText = () => {
+      let contest_input = document.getElementById("contest_input") as HTMLInputElement | null;
+      let team_input = document.getElementById("team_input") as HTMLInputElement | null;
+      if (contest_input == null) return;
+      if (team_input == null) return;
+      window.location.href = "/chart?contest=" + contest_input.value + "&team=" + team_input.value;
+    };
+    return (
+      <div>
         <a href="/">
-            Home
-        </a>
-        <br />
-        {MultiLineChart()}
-    </div>);
+          Home
+        </a><br />
+        <div className="test_box">
+          contest: <input type="text" id="contest_input" defaultValue="standings_2023_domestic" />&quot;standings_2023_domestic&quot; | &quot;standings_2023_yokohama&quot; | &quot;standings_2023_asia&quot;<br />
+          team: <input type="text" id="team_input" defaultValue="KSS908111314" /><br />
+          <button onClick={getText}>グラフを表示</button>
+        </div>
+      </div>
+    );
+  }
+  if (contest == "standings_2023_domestic" || contest == "standings_2023_yokohama" || contest == "standings_2023_asia") {
+  } else {
+    return (
+      <div>
+        <a href="/">
+          Home
+        </a><br />
+        contest: {contest}<br />
+        No such contest exists.
+      </div>
+    );
+  }
+  const len: number = json_data[contest].StandingsData.length;
+  var id: number = -1;
+  for (let i = 0; i < len; i++) {
+    if (json_data[contest].StandingsData[i].TeamName == team) {
+      id = i;
+      break;
+    }
+  }
+  if (id == -1) {
+    return (
+      <div>
+        <a href="/">
+          Home
+        </a><br />
+        contest: {contest}<br />
+        team: {team}<br />
+        No such team exists.
+      </div>
+    );
+  }
+  return Chart(contest, team, id);
+}
+
+export default function Page() {
+  return (
+    <Suspense>
+      <Query />
+    </Suspense>
+  );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,14 @@
 export default function Home() {
   return (
     <div>
-      <a href="/test">
-        Test
-      </a>
-      <br />
       <a href="/chart">
         Chart
-      </a>
-      <br />
-      <a href="/query?contest=standings_2023_domestic&team=KSS908111314">
-        Query
       </a>
       <h1>JSON</h1>
       <ul>
         <li><a href="/standings_2023_domestic.json">ICPC 2023 国内予選</a></li>
         <li><a href="/standings_2023_yokohama.json">ICPC 2023 Asia Yokohama Regional</a></li>
+        <li><a href="/standings_2023_asia.json">2024 ICPC Asia Pacific Championship</a></li>
       </ul>
     </div>
   );

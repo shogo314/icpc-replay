4a86c79ca33934e2c106165683294bfb7cbb7c8e
shogo314 <shg.kawasome@gmail.com>
2024-05-04 03:09:06 +0900

    dir

---
+++ app/json/page.tsx
@@ -0,0 +1,19 @@
+import { contest_data } from "../standings_data";
+export default function Home() {
+  return (
+    <div>
+      <h1>JSON</h1>
+      <div>
+        {
+          (function () {
+            const list = [];
+            for (let i = 0; i < contest_data.length; i++) {
+              list.push(<li><a href={"/standings_" + contest_data[i].id + ".json"}>{contest_data[i].name}</a></li>);
+            }
+            return <ul>{list}</ul>;
+          }())
+        }
+      </div>
+    </div>
+  );
+}

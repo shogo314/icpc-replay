c0bfae8a9285d276e8702e86216279b4029ea990
shogo314 <80336945+shogo314@users.noreply.github.com>
2024-11-21 20:18:04 +0900

    input to select

--- app/standings_data.tsx
+++ app/standings_data.tsx
@@ -1,24 +1,24 @@
 import standings_2022_yokohama from "../public/standings_2022_yokohama.json";
 import standings_2023_domestic from "../public/standings_2023_domestic.json";
 import standings_2023_yokohama from "../public/standings_2023_yokohama.json";
-import standings_2023_asia from "../public/standings_2023_asia.json";
+import standings_2024_asia_pacific from "../public/standings_2024_asia_pacific.json";
 import standings_2024_domestic from "../public/standings_2024_domestic.json";
 import standings_2024_taichung from "../public/standings_2024_taichung.json";
 
 export const contest_data = [
-    { id: "2022_yokohama", name: "ICPC 2022 Asia Yokohama Regional" },
-    { id: "2023_domestic", name: "ICPC 2023 国内予選" },
-    { id: "2023_yokohama", name: "ICPC 2023 Asia Yokohama Regional" },
-    { id: "2023_asia", name: "The 2024 ICPC Asia Pacific Championship" },
-    { id: "2024_domestic", name: "ICPC 2024 国内予選" },
     { id: "2024_taichung", name: "ICPC 2024 Asia Taichung Regional" },
+    { id: "2024_domestic", name: "ICPC 2024 日本 国内予選" },
+    { id: "2024_asia_pacific", name: "The 2024 ICPC Asia Pacific Championship" },
+    { id: "2023_yokohama", name: "ICPC 2023 Asia Yokohama Regional" },
+    { id: "2023_domestic", name: "ICPC 2023 日本 国内予選" },
+    { id: "2022_yokohama", name: "ICPC 2022 Asia Yokohama Regional" },
 ];
 
 export const json_data = {
     "2022_yokohama": standings_2022_yokohama,
     "2023_domestic": standings_2023_domestic,
     "2023_yokohama": standings_2023_yokohama,
-    "2023_asia": standings_2023_asia,
+    "2024_asia_pacific": standings_2024_asia_pacific,
     "2024_domestic": standings_2024_domestic,
     "2024_taichung": standings_2024_taichung,
 } as {
